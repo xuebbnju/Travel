2a0b1c1d104f7870ae0285a9eea3ac061f0a71d4
xuebinging@tucodec.com <xuebinging@tucodec.com>
2018-05-07 15:34:24 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption" >
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl">
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+<script >
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/77/ad7693460cc4de02.jpg_890x330_112cec66.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/d5/fb4a5c81cd87a002.jpg_890x330_901edbfc.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import "~style/varibles.styl"
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  width:100%
+  height:31.25vw
+  .swiper-img
+    width:100%
+</style>
