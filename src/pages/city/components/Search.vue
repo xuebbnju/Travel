6dced14c54a46baf4b37af8a0146f7ee52b8c6d4
xuebinging@tucodec.com <xuebinging@tucodec.com>
2018-05-08 19:38:44 +0800

    searchfinish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,17 +1,60 @@
 <template>
-<div class="search">
-  <input type="text" placeholder="输入城市名或拼音" class="search-input">
+<div>
+  <div class="search">
+  <input type="text" placeholder="输入城市名或拼音" class="search-input" v-model="keyword">
+</div>
+<div class="search-content" ref="search" v-show="keyword">
+  <ul>
+    <li v-for="item of list" :key="item.id" class="search-item border-bottom">{{item.name}}</li>
+    <li v-show="hasNoData" class="search-item border-bottom">没有找到匹配数据</li>
+  </ul>
+</div>
 </div>
 </template>
 <script >
+import Bscroll from 'better-scroll'
 export default {
   name: 'CitySearch',
-  components: {},
-  data () {
-    return {}
+  props: {
+    cities: Object
   },
-  methods: {},
-  mounted () {}
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -29,4 +72,17 @@ export default {
     color: #666
     box-sizing: border-box
     padding: 0 .1rem
+.search-content
+  position: absolute
+  top: 1.5rem
+  left: 0
+  bottom: 0
+  right: 0
+  z-index: 1
+  background: #eee
+  .search-item
+    line-height: .62rem
+    padding-left: .2rem
+    color: #666
+    background: #fff
 </style>
